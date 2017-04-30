8d419ada2c381280605c915b00c40bce1c298908
Dani Porras <daniporras97@gmail.com>
2017-04-29 19:40:25 -0600

    Detalles pequeños de interfaz, y temporizador

--- src/fasttextcontent/MainUI.java
+++ src/fasttextcontent/MainUI.java
@@ -4,19 +4,18 @@ package fasttextcontent;
 import com.teamdev.jxbrowser.chromium.Browser;
 import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Set;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.WindowConstants;
-
 
 public class MainUI implements Observer {
 
@@ -36,7 +35,7 @@ public class MainUI implements Observer {
     
     
     private ArrayList<ContentGetter> resultadosBusqueda;
-    private ArrayList<String> listaPalabrasBusqueda;
+    private Set<String> listaPalabrasBusqueda;
     
     
     public void ejecutarBusqueda(FTC_GUI gui){
@@ -47,13 +46,14 @@ public class MainUI implements Observer {
         
         // El codigo siguiente convierte la entrada del usuario a una lista de las palabras ingresadas
         
-        String stringBusqueda = gui.getStringBusqueda();
+        String stringBusqueda = gui.getStringBusqueda().toLowerCase();
 
         if (!stringBusqueda.equalsIgnoreCase("")){ 
-            listaPalabrasBusqueda = new ArrayList<>(Arrays.asList(stringBusqueda.split(" ")));
+
+            listaPalabrasBusqueda = new HashSet<>(       Arrays.asList(stringBusqueda.split(" "))        );
         }
         else {
-            listaPalabrasBusqueda = new ArrayList<>();
+            listaPalabrasBusqueda = new HashSet<>();
         }
         
         // Termina la conversion
@@ -69,7 +69,12 @@ public class MainUI implements Observer {
         
         if (cache.contiene(palabra)){
             
-            update(null, new ContentGetter( palabra + "2", cache.obtenerValor(palabra) ));
+            double startTime = System.nanoTime();
+            String resultadoCache = cache.obtenerValor(palabra);                    
+            double stopTime = System.nanoTime();
+            
+            String tiempo = String.valueOf((stopTime - startTime)/1000000) + " milisegundos (desde caché).";
+            update(null, new ContentGetter( palabra, resultadoCache, tiempo));
             
         } else {
             ContentGetter getterThread = new ContentGetter(palabra, this);
@@ -81,7 +86,7 @@ public class MainUI implements Observer {
 
     private void limpiarAlmacenamientoLocal() {
         
-        this.listaPalabrasBusqueda = new ArrayList<>();
+        this.listaPalabrasBusqueda = new HashSet<>();
         this.resultadosBusqueda = new ArrayList<>();
         
     }
@@ -92,13 +97,17 @@ public class MainUI implements Observer {
         JFrame frameResultados = new JFrame();
         frameResultados.setLayout(new BorderLayout());
         frameResultados.setSize(700, 700);    
-        frameResultados.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frameResultados.setLocationRelativeTo(null);
+        frameResultados.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        
+        JPanel contenedorInterno = new JPanel();
+        contenedorInterno.setLayout(new BoxLayout(contenedorInterno, BoxLayout.Y_AXIS));
         
         JPanel contenedorExterno = new JPanel();
-        contenedorExterno.setLayout(new BoxLayout(contenedorExterno, BoxLayout.Y_AXIS));
+        contenedorExterno.setLayout(new BorderLayout());
+        contenedorExterno.add(contenedorInterno, BorderLayout.PAGE_START);
         
-        JScrollPane scroll = new JScrollPane(contenedorExterno);
+        JScrollPane scroll = new JScrollPane(contenedorInterno);
         frameResultados.add(scroll, BorderLayout.CENTER);
         
         // Ciclo de insercion de resultados
@@ -109,20 +118,24 @@ public class MainUI implements Observer {
             // Agregar el label con el tiempo
             JPanel panelTiempo = new JPanel(new BorderLayout());
             panelTiempo.add(new JLabel("Tiempo de obtención: " + resultado.getTiempoBusqueda()), BorderLayout.NORTH);
+
             // Cargar el resultado html
             JPanel panelHTML = new JPanel(new BorderLayout());
             Browser browser = new Browser();
             BrowserView htmlViewer = new BrowserView(browser);
-            panelHTML.add(htmlViewer, BorderLayout.CENTER);
-            browser.loadHTML(resultado.getResultadoHTML());
+            panelHTML.add(htmlViewer, BorderLayout.CENTER);  
+            browser.loadHTML(resultado.getResultadoHTML());            
+            panelHTML.setPreferredSize(new Dimension(300,300));
             
             // Agregar todos los componentes a la vista
-            contenedorExterno.add(panelPalabra);
-            contenedorExterno.add(panelTiempo);
-            contenedorExterno.add(panelHTML);           
+            contenedorInterno.add(panelPalabra);
+            contenedorInterno.add(panelTiempo);
+            contenedorInterno.add(panelHTML);
         }
-        
+        frameResultados.pack();
         frameResultados.setVisible(true);
+
     }
+    
 
 }
